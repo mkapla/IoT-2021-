0bc196e62c41b31068151b2b48710bc3fb3f2f57
fokusgc <46088947+fokusgc@users.noreply.github.com>
2021-12-04 18:11:33 +0200

    Update alpha.ino

--- alpha.ino
+++ alpha.ino
@@ -1,9 +1,17 @@
+#include <LiquidCrystal.h>
+#define trigger 18
+#define echo 19
+LiquidCrystal lcd(2,3,4,5,6,7);
+float time=0,distance=0;
+
 void setup() {
   // put your setup code here, to run once:
-pinMode(13,OUTPUT);
+ lcd.begin(16,2);
+ pinMode(trigger,OUTPUT);
+ pinMode(echo,INPUT);
+ lcd.print(" HELLO WORLD");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
